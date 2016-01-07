6fd141f470f4b81f3021aec206172db2c1022e6e
Christian Stocker <me@chregu.tv>
2016-01-07 12:31:12 +0100

    DONT_CHECK_VAGRANTFILE_SAMENESS is not needed anymore

--- provisioning/roles/gitlabci/templates/gitlabci.sh
+++ provisioning/roles/gitlabci/templates/gitlabci.sh
@@ -8,13 +8,6 @@ export CI_TEST_SCRIPT={{ ci_scripts_folder }}run_tests.sh
 
 # export DO_GLOBAL_PROJECTS_CACHE=true
 
-# In the case we have windows hosts, then we copied
-# the Vagrantfile into our root dir and don't have 
-# a symlink, we check later, if they are the same
-# If you don't want this check, uncomment the following
-
-# export DONT_CHECK_VAGRANTFILE_SAMENESS=true 
-
 echo "- Update submodules"
 git submodule update --init
 
