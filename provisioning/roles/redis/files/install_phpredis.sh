f53073e102eafba26bce0514eacf0ec7797d34c7
Christian Stocker <me@chregu.tv>
2016-01-11 13:13:28 +0100

    install php-fpm ini only when it's installed
    and restart php-fpm

--- provisioning/roles/redis/files/install_phpredis.sh
+++ provisioning/roles/redis/files/install_phpredis.sh
@@ -23,11 +23,6 @@ rm -rf phpredis
 sudo sh -c "echo extension=redis.so > /etc/php/7.0/mods-available/redis.ini"
 
 cd /etc/php/7.0/cli/conf.d
-sudo ln -s ../../mods-available/redis.ini
 
-if [[ -d  /etc/php/7.0/fpm/conf.d ]]
-then
-    cd /etc/php/7.0/fpm/conf.d
-    sudo ln -s ../../mods-available/redis.ini
-fi
+sudo ln -fs ../../mods-available/redis.ini
 
