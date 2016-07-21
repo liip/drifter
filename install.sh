776b1a5667c51a100fe10ea5d3c5faef65e917b6
Gilles Crettenand <gilles.crettenand@liip.ch>
2016-07-21 21:12:21 +0200

    oops, forgot to change dir again

--- install.sh
+++ install.sh
@@ -15,6 +15,8 @@ LATEST_COMMIT=$(git rev-list --tags --max-count=1)
 LATEST=$(git describe --tags $LATEST_COMMIT)
 git checkout $LATEST
 
+cd $BASE
+
 cp $VIRTDIR/$REPODIR/provisioning/playbook.yml.dist $VIRTDIR/playbook.yml
 cp $VIRTDIR/$REPODIR/parameters.yml.dist $VIRTDIR/parameters.yml
 cp $VIRTDIR/$REPODIR/ansible.cfg.dist ansible.cfg
