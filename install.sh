75d71bf449af6a9d52ab58057ca7eda085aaedf7
Gilles Crettenand <gilles.crettenand@liip.ch>
2016-07-21 21:08:13 +0200

    Install latest tag instead of master

--- install.sh
+++ install.sh
@@ -10,6 +10,11 @@ mkdir $VIRTDIR
 
 git submodule add https://github.com/liip/drifter.git $VIRTDIR/$REPODIR
 
+cd $VIRTDIR/$REPODIR
+LATEST_COMMIT=$(git rev-list --tags --max-count=1)
+LATEST=$(git describe --tags $LATEST_COMMIT)
+git checkout $LATEST
+
 cp $VIRTDIR/$REPODIR/provisioning/playbook.yml.dist $VIRTDIR/playbook.yml
 cp $VIRTDIR/$REPODIR/parameters.yml.dist $VIRTDIR/parameters.yml
 cp $VIRTDIR/$REPODIR/ansible.cfg.dist ansible.cfg
