e17ce76f0d1834de8138ed6433b08f84b93785cc
Benoît Burgener <benoit.burgener@liip.ch>
2017-05-03 16:29:39 +0200

    Gulp: better Webpack devtool (sourcemaps)

--- provisioning/roles/gulp/templates/Gulpfile.js
+++ provisioning/roles/gulp/templates/Gulpfile.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 /*----------------------------------------*\
   DRIFTER GULPFILE
   Version 1.1.0
@@ -21,7 +22,7 @@ const stripAnsi     = require('strip-ansi');
 /**
  * Watching files for changes
  */
-gulp.task('watch', ['build'], function() {
+gulp.task('watch', ['build'], () => {
   browserSync.init(config.browserSync);
 
   gulp.watch(config.src.sass, ['sass']);
@@ -36,39 +37,39 @@ gulp.task('watch', ['build'], function() {
  * Add vendor prefixes with Autoprefixer
  * Write sourcemaps in dev mode
  */
-gulp.task('sass', function() {
+gulp.task('sass', () => {
   return gulp.src(config.src.sass)
     .pipe($.if(!config.optimize, $.sourcemaps.init()))
     .pipe(
       $.sass(config.sass)
-        .on('error', function(error) {
+        .on('error', error => {
           browserSync.sockets.emit('fullscreen:message', {
             title: 'Sass compilation error',
-            body: error.message
+            body: error.message,
           });
           $.sass.logError.apply(this, arguments);
         })
-        .on('data', function(data) {
+        .on('data', () => {
           browserSync.sockets.emit('fullscreen:message:clear');
         })
     )
     .pipe($.autoprefixer(config.autoprefixer))
     .pipe($.if(!config.optimize, $.sourcemaps.write('.')))
     .pipe(gulp.dest(config.dest.css))
-    .pipe(browserSync.stream({match: '**/*.css'}));
+    .pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
 {% if gulp_use_webpack %}
 /**
  * Bundle JavaScript modules
  */
-gulp.task('webpack', function(done) {
-  webpack.run(function(error, stats) {
+gulp.task('webpack', done => {
+  webpack.run((error, stats) => {
     if (stats.hasErrors() || stats.hasWarnings()) {
       browserSync.sockets.emit('fullscreen:message', {
         title: 'WebPack compilation error',
         body: stripAnsi(stats.toString()),
-        timeout: 100000
+        timeout: 100000,
       });
       $.util.log('[webpack]', stats.toString());
     } else {
@@ -83,13 +84,13 @@ gulp.task('webpack', function(done) {
 /**
  * Optimize images
  */
-gulp.task('images', function () {
+gulp.task('images', () => {
   return gulp.src(config.src.images)
     .pipe($.imagemin({
       progressive: true,
       svgoPlugins: [{
-        removeViewBox: false
-      }]
+        removeViewBox: false,
+      }],
     }))
     .pipe(gulp.dest(config.dest.images));
 });
--- provisioning/roles/gulp/templates/gulp.config.js
+++ provisioning/roles/gulp/templates/gulp.config.js
@@ -1,33 +1,34 @@
+/* eslint-env node */
 const argv = require('yargs').argv;
 
 module.exports = {
-  optimize:         argv.production,
+  optimize: argv.production,
   src: {
-    sass:           'static/sass/**/*.scss',
-    images:         'static/images/**/*.{gif,jpg,jpeg,png,svg}',
-    javascripts:    'static/javascripts/**/*.js',
-    templates:      '**/*.html'
+    sass: 'static/sass/**/*.scss',
+    images: 'static/images/**/*.{gif,jpg,jpeg,png,svg}',
+    javascripts: 'static/javascripts/**/*.js',
+    templates: '**/*.html',
   },
   dest: {
-    css:            'static/stylesheets',
-    images:         'static/images'
+    css: 'static/stylesheets',
+    images: 'static/images',
   },
   browserSync: {
-    proxy:          '{{ hostname }}',
+    proxy: '{{ hostname }}',
 {% if ssl|default(false) and ssl_key_file is defined and ssl_cert_file is defined %}
     https: {
       key: '{{ ssl_key_file }}',
-      cert: '{{ ssl_cert_file }}'
+      cert: '{{ ssl_cert_file }}',
     },
 {% endif %}
-    open:           false,
-    notify:         false,
-    plugins:        ['bs-pretty-message']
+    open: false,
+    notify: false,
+    plugins: ['bs-pretty-message'],
   },
   sass: {
-    outputStyle:    'compressed'
+    outputStyle: 'compressed',
   },
   autoprefixer: {
-    cascade:        false
-  }
+    cascade: false,
+  },
 };
